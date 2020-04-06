d04bb21b9ce9d42d3134583bce7016032c69d099
S-Rad <shima.radfar@txs.de>
2020-04-06 14:43:46 +0200

    add boxes for to dos, edit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import store from './app/store';
-import App from './App';
+import React from "react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+test("renders correct text", () => {
+  const { getByText } = global.wrapComponent(<App />);
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText("Unsorted To Dos")).toBeInTheDocument();
+  expect(getByText("Finished To Dos")).toBeInTheDocument();
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,14 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import "@testing-library/jest-dom/extend-expect";
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import store from "./app/store";
+
+
+global.beforeEach(() => {
+  global.wrapComponent = (children) =>
+    render(<Provider store={store}>{children}</Provider>);
+});
