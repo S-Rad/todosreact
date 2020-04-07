a8bb799be40e762560fea71bdae93925cbf97769
S-Rad <shima.radfar@txs.de>
2020-04-07 12:00:49 +0200

    added styles, resort boxes with screen width

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from "react";
-import { Box } from "@material-ui/core";
+import { Box, makeStyles } from "@material-ui/core";
 import "./App.css";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+    },
+    [theme.breakpoints.up("md")]: {
+      flexDirection: "row",
+    },
+  },
+}));
+
+const App = () => {
+  //styles nach oben ziehen
+  const classes = useStyles();
   return (
-    <Box display="flex" m={1}>
-      <Box mr={2}>
+    <Box className={classes.root} display="flex" m={1}>
+      <Box mr={2} flexGrow={1}>
         <Box>Unsorted To Dos aaaaaaaaaaaaa</Box>
         <Box>Finished To Dos</Box>
       </Box>
-      <Box>Eisenhower To Dos</Box>
+      <Box flexGrow={2}>Eisenhower To Dos</Box>
     </Box>
   );
-}
+};
 
 export default App;
