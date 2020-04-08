9b208b365fe864e74054200168e850ec89a24940
S-Rad <S-Rad@users.noreply.github.com>
2020-04-08 09:30:28 +0000

    deploy: 7298ac530d51594050b848a4b8ff054fb9e7e7bc

---
+++ precache-manifest.130468e52d85d13dfbc6c18cf59e52d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5652c1276b9b35d02abc004fb4831922",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8364f16558fdba3c266",
+    "url": "/static/js/2.97692a07.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.97692a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5574d6e48ca09165cc34",
+    "url": "/static/js/main.cc3bc0ab.chunk.js"
+  },
+  {
+    "revision": "2b97fa0b0aa07f6cb32b",
+    "url": "/static/js/runtime-main.c9388d5c.js"
+  }
+]);
