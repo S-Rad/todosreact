eb7eb1fa2533f741868628ffb1d8ec4154df9aa7
S-Rad <shima.radfar@txs.de>
2020-04-08 13:44:54 +0200

    matrix fields displayed correctly

--- src/components/RightMatrix.js
+++ src/components/RightMatrix.js
@@ -1,10 +1,29 @@
 import React from "react";
-import { Box } from "@material-ui/core";
+import { Box, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  wrapper: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  matrixfield: {
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+    },
+    [theme.breakpoints.up("md")]: {
+      width: "50%",
+    },
+  },
+}));
 
 const RightMatrix = () => {
+  const classes = useStyles();
   return (
-    <Box m={2} flexGrow={2}>
-      Eisenhower To Dos
+    <Box m={2} flexGrow={2} className={classes.wrapper}>
+      <Box className={classes.matrixfield}>Important, Urgent</Box>
+      <Box className={classes.matrixfield}>Important, Not Urgent</Box>
+      <Box className={classes.matrixfield}>Not Important, Urgent</Box>
+      <Box className={classes.matrixfield}>Not Important, Not Urgent</Box>
     </Box>
   );
 };
