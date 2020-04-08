c84fd24387f1ae3405b7154ec33f905131cbbe3a
S-Rad <shima.radfar@txs.de>
2020-04-08 14:18:41 +0200

    added button to add To Dos

--- src/components/AddToDoField.js
+++ src/components/AddToDoField.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Box, TextField } from "@material-ui/core";
+import { Box, TextField, Button } from "@material-ui/core";
 
 const AddToDoField = () => {
   return (
     <Box flexDirection="row">
       <TextField id="filled-basic" label="Filled" variant="filled" />
+      <Button variant="contained">Add</Button>
     </Box>
   );
 };
