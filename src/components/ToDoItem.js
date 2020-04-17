91aa23bd5f1221d91a09b7cd48f9d23883d592df
S-Rad <shima.radfar@txs.de>
2020-04-17 13:37:59 +0200

    added activity state to todoitem

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Checkbox, Button } from "@material-ui/core";
 
 const ToDoItem = () => {
+  const [active, setActive] = useState("true");
+  const activeStyle = {
+    color: "#000000",
+    textDecoration: "none",
+  };
+  const inactiveStyle = {
+    color: "#A9A9A9",
+    textDecoration: "line-through",
+  };
+
+  const handleChecked = () => {
+    setActive(!active);
+  };
+
   return (
     <Box display="flex" alignItems="center" justifyContent="space-between">
       <Box>
-        <Checkbox />
+        <Checkbox onClick={handleChecked} />
         To Do
       </Box>
       <Button variant="contained">Yeet</Button>
