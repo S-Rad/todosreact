5752d14ecbadb6557cbd1f9a8d498698056e25c9
S-Rad <shima.radfar@txs.de>
2020-04-22 10:55:00 +0200

    todos can be added

--- src/components/AddToDoField.js
+++ src/components/AddToDoField.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, TextField, Button } from "@material-ui/core";
 
 const AddToDoField = (props) => {
+  const [toDoText, setToDoText] = useState("");
+
   return (
     <Box display="flex" flexDirection="row">
-      <TextField id="filled-basic" label="Filled" variant="filled" fullWidth />
-      <Button variant="contained" onClick={() => props.onClick("meow")}>
+      <TextField
+        id="filled-basic"
+        label="Filled"
+        variant="filled"
+        onChange={(event) => setToDoText(event.target.value)}
+        fullWidth
+      />
+      <Button variant="contained" onClick={() => props.onClick(toDoText)}>
         +
       </Button>
     </Box>
   );
 };
-
 export default AddToDoField;
