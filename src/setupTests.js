07dc4d182e5408a8e4e1764659fbf25246b453a3
S-Rad <shima.radfar@txs.de>
2020-10-12 14:25:01 +0200

    fixed store path setupTests

--- src/setupTests.js
+++ src/setupTests.js
@@ -6,8 +6,7 @@ import "@testing-library/jest-dom/extend-expect";
 import React from "react";
 import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
-import store from "./app/store";
-
+import store from "./store";
 
 global.beforeEach(() => {
   global.wrapComponent = (children) =>
