6d97bf676863c0c0b4112f0bb2536f4feef78446
S-Rad <shima.radfar@txs.de>
2020-12-08 15:39:01 +0100

    todos categorize correctly

--- src/components/RightMatrix.js
+++ src/components/RightMatrix.js
@@ -6,6 +6,8 @@ import {
   category_b1,
   category_b2,
 } from "../locale.json";
+import { useSelector } from "react-redux";
+import { ToDoList } from ".";
 
 const useStyles = makeStyles((theme) => ({
   wrapper: {
@@ -24,12 +26,33 @@ const useStyles = makeStyles((theme) => ({
 
 const RightMatrix = () => {
   const classes = useStyles();
+  const { todos } = useSelector((_) => _);
   return (
     <Box m={2} flexGrow={2} className={classes.wrapper}>
-      <Box className={classes.matrixfield}>{category_a1}</Box>
-      <Box className={classes.matrixfield}>{category_a2}</Box>
-      <Box className={classes.matrixfield}>{category_b1}</Box>
-      <Box className={classes.matrixfield}>{category_b2}</Box>
+      <Box className={classes.matrixfield}>
+        {category_a1}
+        <ToDoList
+          todos={todos.filter((toDo) => toDo.active && toDo.category === "a1")}
+        />
+      </Box>
+      <Box className={classes.matrixfield}>
+        {category_a2}
+        <ToDoList
+          todos={todos.filter((toDo) => toDo.active && toDo.category === "a2")}
+        />
+      </Box>
+      <Box className={classes.matrixfield}>
+        {category_b1}
+        <ToDoList
+          todos={todos.filter((toDo) => toDo.active && toDo.category === "b1")}
+        />
+      </Box>
+      <Box className={classes.matrixfield}>
+        {category_b2}
+        <ToDoList
+          todos={todos.filter((toDo) => toDo.active && toDo.category === "b2")}
+        />
+      </Box>
     </Box>
   );
 };
